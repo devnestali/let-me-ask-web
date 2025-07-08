279a776f012170bcd371f9278593f6a43767cfe1
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-07-09 00:11:28 +0200

    build: Configuring react-router-dom to handle routes between pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { Room } from './pages/room'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<CreateRoom />} index />
+        <Route element={<Room />} path="/room" />
+      </Routes>
+    </BrowserRouter>
+  )
 }
