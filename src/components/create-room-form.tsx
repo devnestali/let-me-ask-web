5d6ded38b4cf92d598f3bdeca635771498cc1c48
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-07-09 23:52:32 +0200

    feat: Creating visual structure on create-room-form component

---
+++ src/components/create-room-form.tsx
@@ -0,0 +1,105 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod/v4'
+import { Button } from './ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+
+const createRoomFormSchema = z.object({
+  name: z.string().min(1, { message: 'Incluya en mínimo 3 caracteres' }),
+  description: z.string(),
+})
+
+type CreateRoomFormData = z.infer<typeof createRoomFormSchema>
+
+export function CreateRoomForm() {
+  const createRoomForm = useForm<CreateRoomFormData>({
+    resolver: zodResolver(createRoomFormSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  })
+
+  function handleCreateRoom(data: CreateRoomFormData) {
+    // biome-ignore lint/suspicious/noConsole: tu é chato demais biome, mas obrigado por ser assim
+    console.log(data)
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Crear sala</CardTitle>
+        <CardDescription>
+          Crea una nueva sala para empezar a hacer preguntas y recibir
+          respuestas de la I.A
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...createRoomForm}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={createRoomForm.handleSubmit(handleCreateRoom)}
+          >
+            <FormField
+              control={createRoomForm.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Nombre de la sala</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Ingrese el nombre de la sala..."
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )
+              }}
+            />
+
+            <FormField
+              control={createRoomForm.control}
+              name="description"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Descripcion de la sala</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        {...field}
+                        placeholder="Ingrese una descripción sobre la sala..."
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )
+              }}
+            />
+
+            <Button className="w-full cursor-pointer" type="submit">
+              Crear sala
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
+}
